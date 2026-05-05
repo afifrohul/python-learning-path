912ac10dd33e096c812d726443e420c94437e614
afifrohul <afifmemyself22@gmail.com>
2026-05-05 08:40:12 +0700

    feat(open-shop-app-back-end): add soft delete method

--- Belajar Back-End Pemula dengan Python/open-shop-app-back-end/products/views.py
+++ Belajar Back-End Pemula dengan Python/open-shop-app-back-end/products/views.py
@@ -29,3 +29,17 @@ class ProductDetail(APIView):
     product = self.get_object(pk)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+  
+  def put(self, request, pk):
+    product = self.get_object(pk)
+    serializer = ProductSerializer(product, data=request.data, context={'request': request})
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  def delete(self, request, pk):
+    product = self.get_object(pk)
+    product.is_delete = True
+    product.save()
+    return Response(status=status.HTTP_204_NO_CONTENT)
