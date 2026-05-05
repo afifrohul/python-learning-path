9985f7fc770316c3bc88d45a6898ae497d8125ac
afifrohul <afifmemyself22@gmail.com>
2026-05-05 13:40:23 +0700

    feat(movie-go-app-back-end): add CRUD reservations

--- Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/movie_go/urls.py
+++ Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/movie_go/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api/', include('studios.urls')),
     path('api/', include('seats.urls')),
     path('api/', include('showtimes.urls')),
+    path('api/', include('reservations.urls')),
 ]
