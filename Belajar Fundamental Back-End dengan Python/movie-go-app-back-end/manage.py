7dc1722a6214207d7eb3ad06fe23d5da91dffda0
afifrohul <afifmemyself22@gmail.com>
2026-05-07 19:42:16 +0700

    feat(movie-go-app-back-end): using sentry for logging

--- Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/manage.py
+++ Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/manage.py
@@ -3,19 +3,13 @@
 import os
 import sys
 from loguru import logger
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.loguru import LoguruIntegration
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_go.settings.dev')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
 
     logger.remove()  # hapus handler bawaan
     logger.add(sys.stdout, level="INFO")  # log ke console
@@ -27,7 +21,34 @@ def main():
     logger.add("logs/app.log",
             rotation="1 day",  # rotasi harian
             level="INFO") # hanya log level info
+    
+    sentry_sdk.init(
+        dsn=os.getenv("SENTRY_DSN"),
+        integrations=[
+            DjangoIntegration(),
+            LoguruIntegration(
+                level="INFO",        # Minimum level for breadcrumbs
+                event_level="ERROR"  # Minimum level to create a Sentry event
+            ),
+        ],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # If you wish to send personal data like user IDs
+        send_default_pii=True
+    )
 
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+   
 
 if __name__ == '__main__':
     main()
--- Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/movie_go/settings/prod.py
+++ Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/movie_go/settings/prod.py
@@ -1,8 +1,67 @@
 import os
+import sys
+
 from .common import *
+
+from loguru import logger
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.loguru import LoguruIntegration
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '34.128.96.165'
+]
+
+# =========================
+# SENTRY
+# =========================
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+
+    integrations=[
+        DjangoIntegration(),
+
+        LoguruIntegration(
+            level="INFO",
+            event_level="ERROR",
+        ),
+    ],
+
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
+
+# =========================
+# LOGURU
+# =========================
+
+logger.remove()
+
+# Console log
+logger.add(
+    sys.stdout,
+    level="INFO",
+)
+
+# Error log file
+logger.add(
+    "logs/error.log",
+    rotation="500 MB",
+    level="ERROR",
+    backtrace=True,
+    diagnose=True,
+)
+
+# App log file
+logger.add(
+    "logs/app.log",
+    rotation="1 day",
+    level="INFO",
+)
