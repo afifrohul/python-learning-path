b33d6b8ad89f902208a92d000ba5c1c1ccb60467
afifrohul <afifmemyself22@gmail.com>
2026-05-07 11:13:19 +0700

    feat(movie-go-app-back-end): adjust movie poster logic upload

--- Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/movies/models.py
+++ Belajar Fundamental Back-End dengan Python/movie-go-app-back-end/movies/models.py
@@ -23,7 +23,7 @@ class Movie(models.Model):
 class MoviePoster(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='public/movie_posters/')
  
     def __str__(self):
         return self.movie.name
